151be86b91e11d4eb2bac41e2b52588c1f4acc53
AndrewSchatz <celestiongames@gmail.com>
2026-07-05 09:07:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Landing page carregada!");
+const botao = document.getElementById("topo");
+
+window.addEventListener("scroll",()=>{
+if(window.scrollY>300){
+botao.style.display="block";
+}else{
+botao.style.display="none";
+}
+});
+
+botao.addEventListener("click",()=>{
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+});
